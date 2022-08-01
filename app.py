56bd98272bdc7900bc6104a3b6e0cd465826e065
Howard Halim <howardhalim@gmail.com>
2022-08-01 13:25:39 -0400

    add model.pkl and cat-dog classifier

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 import gradio as gr
+from fastai.vision.all import *
 
-def greet(name):
-    return "Hello " + name + "!!"
+def is_cat(x): return x[0].isupper()
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+learn = load_learner('model.pkl')
+
+labels = learn.dls.vocab
+print(labels)
+
+def predict(img):
+    img = PILImage.create(img)
+    pred, pred_idx, probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+iface = gr.Interface(
+    fn=predict,
+    inputs=gr.Image(source='webcam', shape=(192,192)),
+    outputs=gr.Label(num_top_classes=3)
+)
+iface.launch(share=True)
